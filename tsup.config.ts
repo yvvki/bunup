4d04c9694826469455a1ec93221637f67718ada4
arshad <arshadpyaseen@gmail.com>
2025-03-19 11:32:33 +0530

    chore: update

---
+++ tsup.config.ts
@@ -0,0 +1,31 @@
+import {defineConfig} from 'tsup';
+
+export default defineConfig([
+    {
+        name: 'bunup',
+        outDir: 'build',
+        entry: ['./src/index.ts'],
+        format: ['cjs', 'esm'],
+        dts: true,
+        minify: true,
+        splitting: true,
+    },
+    {
+        name: 'bunup-cli',
+        outDir: 'build',
+        entry: ['./src/cli.ts'],
+        format: ['esm'],
+        minify: true,
+        splitting: true,
+    },
+    {
+        name: 'bunup-dts-worker',
+        outDir: 'build',
+        format: ['cjs'],
+        entry: {
+            dtsWorker: './src/dts-worker.ts',
+        },
+        minify: true,
+        splitting: true,
+    },
+]);
