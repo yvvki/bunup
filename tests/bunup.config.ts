ada8b627cd2d02b698875d4727629a39a8b68e62
arshad <arshadpyaseen@gmail.com>
2025-03-31 01:37:18 +0530

    chore: remove unused dists

--- tests/bunup.config.ts
+++ tests/bunup.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'bunup';
 
 export default defineConfig({
-      entry: ['src/index.ts', 'src/index.ts'],
+      entry: ['src/index.ts', 'src/some.ts'],
       format: ['esm', 'cjs'],
       minify: true,
       dts: true,
