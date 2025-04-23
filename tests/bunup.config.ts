1ae7dfb97f7e275b4ec4deb05c3fe57ea21464eb
arshad <arshadpyaseen@gmail.com>
2025-04-24 01:20:26 +0530

    docs: workspace docs update

--- tests/bunup.config.ts
+++ tests/bunup.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from "../dist/index.mjs";
+import { defineWorkspace } from "../dist/index.mjs";
 
-export default defineConfig({
-    entry: ["fixtures/index.ts"],
-    format: ["esm", "cjs"],
-    dts: true,
-});
+export default defineWorkspace([
+    {
+        name: "client",
+        root: "fixtures/client",
+        config: {
+            entry: ["index.ts"],
+            format: ["esm", "cjs"],
+            dts: true,
+        },
+    },
+    {
+        name: "server",
+        root: "fixtures/server",
+        config: {
+            entry: ["index.ts"],
+            format: ["esm", "cjs"],
+            outDir: "dist/server",
+            dts: true,
+        },
+    },
+]);
